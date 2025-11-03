bbaf03ffce2c44c39d3ad6f7bf4a91df13a5f267
neil930220 <neil930220@gmail.com>
2025-11-03 11:49:29 +0800

    fuck

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -16,6 +16,17 @@ export default defineConfig({
   },
   define: {
     '__DEFINES__': JSON.stringify({}),
+    '__BASE__': JSON.stringify('/'),
+    '__SERVER_HOST__': JSON.stringify('http://localhost:8000'),
+    '__WS_TOKEN__': JSON.stringify(null),
+    '__HMR_CONFIG_NAME__': JSON.stringify('default'),
+    '__HMR_PROTOCOL__': JSON.stringify('wss'),
+    '__HMR_HOSTNAME__': JSON.stringify(null),
+    '__HMR_PORT__': JSON.stringify(null),
+    '__HMR_DIRECT_TARGET__': JSON.stringify(null),
+    '__HMR_BASE__': JSON.stringify('/'),
+    '__HMR_TIMEOUT__': JSON.stringify(30000),
+    '__HMR_ENABLE_OVERLAY__': JSON.stringify(true),
     'process.env': {}
   },
   server: {
